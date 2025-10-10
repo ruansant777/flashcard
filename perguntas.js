159d0bdee037e79cbec4aad9799990683e93839c
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-10-10 09:24:10 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,35 @@
+criaCartao (
+    'Futebol',
+    'Qual o melhor jogador',
+    'Neymar'
+)
 
+criaCartao (
+    'Matemática',
+    'Qual número de pi?',
+    '3,14'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é o maior deserto do mundo?',
+    'Deserto do Saara'
+)
+
+criaCartao (
+    'Química',
+    'Qual nome do elemento que possui H2O na fórmula',
+    'Água'
+)
+
+criaCartao (
+    'Categoria',
+    'Pergunta',
+    'Reposta'
+)
+
+criaCartao (
+    'Categoria',
+    'Pergunta',
+    'Reposta'
+)
