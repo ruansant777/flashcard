5c16ea17c315f79c53514c25c1a2dc1ff589dc41
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-10-17 08:17:47 -0300

    .

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-}
 
-cartao.innerHTML =
+
+cartao.innerHTML = `
     <div class="cartao_conteudo">
-                  <h3>Programação<h3>
-                  <div> class="cartao_conteudo_pergunta">
-                       <p>O que é Javascript?</p>
+                  <h3>${categoria}<h3>
+                  <div class="cartao_conteudo_pergunta">
+                       <p>${pergunta}</p>
                  </div>
                  <div class="cartao_conteudo_resposta">
-                       <p>O javaScript é uma linguagem de Programação</p>
+                       <p>${resposta}</p>
                  </div>
               </div>
-        
+              `
+    container.appendChild(cartao)
+}
